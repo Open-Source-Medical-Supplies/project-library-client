294f6f14b94f3b01921c4974aadcefa6d78031e8
jtorreggiani <joetorreggiani@gmail.com>
2025-04-30 21:04:03 -0400

    Remove extra import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import { Search } from 'lucide-react';
 import CategoryCard from './components/CategoryCard';
 import ProjectCard from './components/ProjectCard';
